54542b3953adec7a892580b97885fd852ed2dd04
kim <jayfulkr@gmail.com>
2023-12-06 17:47:35 +0900

    gitattributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import './App.css';
-import Footer from './components/Footer';
 import MainPage from './components/MainPage';
-// import Navbar from './components/Navbar';
 import Commercial from './components/Commercial';
 import Horizontal from './components/Horizontal';
 import Reason from './components/Reason';
 import SampleUi from './components/SampleUi';
 import VideoContent from './components/VideoContent';
 import Ending from './components/Ending';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='w-screen '>
-      {/* <Navbar /> */}
-      {/* <div className='max-w-full bg-gradient-to-b from-[#0f1025] via-[#0f1025] to-[#0f1025]'> */}
       <div className='max-w-full bg-[#303560]'>
         <MainPage />
         <Commercial />
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,20 +1,7 @@
-// import { useEffect, useState } from 'react';
-// import { gsap } from 'gsap';
-
 const MainPage = () => {
   return (
     <div className='relative h-screen w-full'>
-      {/* <img
-        className='w-[200px] absolute left-8 top-8'
-        src='/public/logo.svg'
-        alt=''
-      /> */}
-      {/* <button className='sticky top-12 left-full rounded-full border border-white text-white px-4 py-2 text-xs hover:border-PURPLE hover:bg-PURPLE hover:text-white hover:scale-110 duration-300'>
-        DOWNLOAD
-      </button> */}
-      <div className='w-full h-full flex justify-center items-center bg-[url("/public/main.png")] bg-cover bg-no-repeat bg-center'>
-        {/* <h1 className='text-7xl font-bold text-white z-10'>MainPage</h1> */}
-      </div>
+      <div className='w-full h-full flex justify-center items-center bg-[url("/public/main.png")] bg-cover bg-no-repeat bg-center'></div>
     </div>
   );
 };
