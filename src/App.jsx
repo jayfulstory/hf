16a518c58f2fffedd526fe8b3bb40cd1332fc1b3
kim <jayfulkr@gmail.com>
2023-12-12 21:19:30 +0900

    12/12 update

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,16 @@ import VideoContent from './components/VideoContent';
 import Ending from './components/Ending';
 import Footer from './components/Footer';
 import { useState, useEffect } from 'react';
-import imagesLoaded from 'imagesloaded';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const handleWindowLoad = () => {
-    setIsLoading(false);
     window.scrollTo(0, 0);
+    setIsLoading(false);
   };
   // const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    const img = document.querySelectorAll('img');
-    const getProgress = instance => {
-      // setProgress(Math.round((instance.progressedCount * 100) / img.length));
-
-      console.log(
-        'progress',
-        Math.round((instance.progressedCount * 100) / img.length)
-      );
-    };
-
-    imagesLoaded(img).on('progress', getProgress);
-
     if (document.readyState === 'complete') {
       handleWindowLoad();
     }
@@ -42,8 +29,7 @@ function App() {
   }, []);
 
   return (
-    <div className={`w-screen ${isLoading && 'h-screen'}`}>
-      {/* {isLoading && <Loading isLoading={isLoading} />} */}
+    <div className={`w-screen `}>
       <Loading isLoading={isLoading} />
       <MainPage />
       <div className='max-w-full bg-gradient-to-b from-DARK_BLUE to-BLUE'>
--- src/components/VideoContent.jsx
+++ src/components/VideoContent.jsx
@@ -1,8 +1,17 @@
+import YouTube from 'react-youtube';
+
 const VideoContent = () => {
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <div className='w-screen h-screen font-bold text-white flex justify-center items-center'>
-      <div className='bg-orange-400 w-1/2 h-1/2 flex justify-center items-center'>
-        <h2 className='text-7xl font-bold text-white '>VideoContent</h2>
+      <div className=' w-1/2 h-1/2 flex justify-center items-center'>
+        <YouTube videoId='0ghIVa_VWg0' otps={opts} />
       </div>
     </div>
   );
